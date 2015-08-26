cc22aa3ddde4ea99fc30b8dbc84c3ad30cc7e4a1
NiveditN <niveditn@gmail.com>
2015-08-26 13:46:56 +0000

    Tried async waterfall

--- server/lib/routes/socket.js
+++ server/lib/routes/socket.js
@@ -1,4 +1,5 @@
 var cv = require('opencv');
+var async = require('async');
 
 // camera properties
 var camWidth = 320;
@@ -17,15 +18,79 @@ camera.setHeight(camHeight);
 
 module.exports = function (socket) {
   setInterval(function() {
+
+    async.waterfall([
+        function(callback) {
+
+            console.log('read camera')
+            camera.read(function (err, im) {
+              if (err) return callback(err);
+
+              if(!im) return;
+              var size = im.size();
+              if(!size[0]) return;
+              if(!size[1]) return;  
+
+              callback(null, im);
+            });
+                   
+        },
+        
+        function(im, callback) {
+
+          console.log('detect object');
+          im.detectObject('./node_modules/opencv/data/haarcascade_frontalface_alt2.xml', {}, function(err, faces) {
+            if (err) return callback(err)
+
+            console.log('draw face');
+            for (var i = 0; i < faces.length; i++) {
+              face = faces[i];
+              im.rectangle([face.x, face.y], [face.width, face.height], rectColor, rectThickness);
+            }
+
+            console.log('emit socket');
+            socket.emit('frame', { buffer: im.toBuffer() });
+
+            callback(null, im);
+
+            /* Putting it here results in segmentation fault */
+
+          });
+
+          // callback(null, im);
+
+          /* Putting this here results in the following error, as expected */
+          /*
+          OpenCV Error: Unknown error code -10 
+          (Raw image encoder error: Empty JPEG image (DNL not supported)) in throwOnEror, 
+          file /home/pi/installers/opencv-2.4.10/modules/highgui/src/grfmt_base.cpp, line 131
+          terminate called after throwing an instance of 'cv::Exception'
+
+          what():  /home/pi/installers/opencv-2.4.10/modules/highgui/src/grfmt_base.cpp:131: 
+          error: (-10) Raw image encoder error: Empty JPEG image (DNL not supported) in function throwOnEror
+
+          Aborted 
+          */
+
+        },
+
+        function(im, callback) {
+          console.log("release im")
+          if(im != null || im != {}) {
+            im.release();            
+          }
+        }
+
+    ], function (err, im) {
+        if(err) return console.log(err);
+        console.log("done");
+    });
+
+    /*
+
     camera.read(function(err, im) {
-      
       if (err) throw err;
 
-      if(!im) return;
-      var size = im.size();
-      if(!size[0]) return;
-      if(!size[1]) return;
-      
       im.detectObject('./node_modules/opencv/data/haarcascade_frontalface_alt2.xml', {}, function(err, faces) {
         if (err) throw err;
 
@@ -35,10 +100,11 @@ module.exports = function (socket) {
         }
 
         socket.emit('frame', { buffer: im.toBuffer() , data: im });
-	im.release()
+        im.release()
       });
-
     });
+
+    */
 
   }, camInterval);
 };
